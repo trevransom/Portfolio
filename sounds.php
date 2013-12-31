f058137da50c5c5b155e6a0757e28cb7e8e5ce1c
Trevor Ransom <trev.ransom@me.com>
2013-12-30 18:31:20 -0800

    Medium and large css for Sounds

--- sounds.php
+++ sounds.php
@@ -4,22 +4,24 @@ include "header.php";
 ?>
 
 	<body>
-		<div id="sounds">
-			<div id="sounds_album_cover_position">
+		<div class="sounds">
+			<div id="sounds_album_cover">
 				<p class="sound_title">Download</p>
-				<a href="http://www.mediafire.com">
-					<p class="sounds_album_cover_title">Hercules</p> 
-					<img class="sounds_album_cover" src="img/Cover Photo Concept Rendition.jpg">
-				</a>
-			</div>
-	
-	
-			<div id="stream">
-				<p class="sound_title">Stream</p>
-				<div id="soundcloud_player">
-					<iframe width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/43501429&amp;color=15b1ed&amp;auto_play=false&amp;show_artwork=true"></iframe>
+				<div id="sounds_album_link">
+					<a href="http://mediafire.com">
+						<p class="sounds_album_cover_title">Hercules</p>
+						<img id="sounds_album_pic" src="img/Cover Photo Concept Rendition.jpg">
+					</a>
 				</div>
 			</div>
+
+			<div id="stream_album">
+				<p class="sound_title" style="position:relative; left:45%;">Stream</p>
+				<div id="soundcloud_player">
+					<iframe width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/43501429&amp;color=15b1ed&amp;auto_play=false&amp;show_artwork=true"></iframe>
+				</div>	
+			</div>
 		</div>
+
 	</body>
 </html>
